1378cf1f867b2e99c377aabbdb165558e38cf3ae
maiosha99 <maimahran9899@gmail.com>
2025-10-01 21:24:20 +0300

    Update getImagePrefix.ts

--- src/utils/getImagePrefix.ts
+++ src/utils/getImagePrefix.ts
@@ -1,8 +1,7 @@
 export function getImagePrefix() {
   if (process.env.NODE_ENV === "production") {
-    // لو انتِ على GitHub Pages
+   
     return "/Crypgo/";
   }
-  // لو انتِ على Vercel أو local
   return "/";
 }
