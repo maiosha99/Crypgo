474079a7b5adb8bd6e114300e78906f63003018e
maiosha99 <maimahran9899@gmail.com>
2025-10-01 19:57:56 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
-/** @type {import('next').NextConfig} */
+//  @type {import('next').NextConfig} 
 const isProd = process.env.NODE_ENV === "production";
+const isVercel = !!process.env.VERCEL; // عشان يفرّق بين Vercel و GitHub Pages
 
 const nextConfig = {
-  basePath: isProd ? "/Crypgo" : "",
-  assetPrefix: isProd ? "/Crypgo/" : "",
+  basePath: isProd && !isVercel ? "/Crypgo" : "",
+  assetPrefix: isProd && !isVercel ? "/Crypgo/" : "",
   output: "export",
   images: {
     unoptimized: true,
@@ -11,5 +12,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
- 
